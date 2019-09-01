80a568c642c96482893745ceddaf4c8a8ad42ade
Ivan Pavluk <ivanpavluk00@gmail.com>
2019-09-01 16:13:42 +0700

    how did this even happen

--- osu/collections.py
+++ osu/collections.py
@@ -1,70 +1,53 @@
-# Before running the script, run this command: pip install osu.py tqdm
+from .utility import BinaryFile
 
-import osu, os, shutil
-try:
-	import tqdm
-except:
-	tqdm = None
+class Collection:
+	def __init__(self, **kwargs):
+		self.name = kwargs.get('name', '')
+		self.hashes = kwargs.get('hashes', [])
 
-# Config start. But the filters themselves are configured at line 58!
-# Use double backslash
-INPUT = 'C:\\osu!'
-OUTPUT = 'C:\\Users\\pavlukivan\\code\\tmp'
-# You can replace "None" with some collection name (but make sure to quote it)
-COLLECTION = None
-MODE = osu.Mode.STD #you can do TAIKO, MANIA, CTB
-MODS = osu.Mods()
-# Remove the # from some of the following lines calculate SR for these mods instead. You can combine multiple lines, like enable both DT and HR to use SR calculated for DTHR.
-#MODS.EZ = True
-#MODS.HT = True
-#MODS.HR = True
-#MODS.DT = True
-# Config end
+	@classmethod
+	def fromDatabase(cls, colldb):
+		self = cls()
+		self.name = colldb.readOsuString()
+		bmCount = colldb.readInt()
+		self.hashes = []
+		for i in range(bmCount):
+			self.hashes.append(colldb.readOsuString())
+		return self
 
-print('Loading osu!.db.')
-db=osu.OsuDb(os.path.join(INPUT, 'osu!.db'))
-
-if COLLECTION:
-	colls={coll.name: coll.hashes for coll in osu.CollectionDb(os.path.join(INPUT, 'collection.db'))}
-	if not COLLECTION in colls.keys():
-		print("Error: couldn't find collection ", COLLECTION, '. Check your collection is EXACTLY the same as in-game (case-sensitive).', sep='')
-
-dirs=set()
-
-print('Loading finished. Filtering the beatmaps...')
-
-for map in db.beatmaps:
-	if map.mode != MODE or int(MODS) not in map.SR[map.mode].keys():
-		continue
-	if COLLECTION and map.hash not in colls[COLLECTION]:
-		continue
+	def writeToDatabase(self, colldb):
+		colldb.writeOsuString(self.name)
+		colldb.writeInt(len(self.hashes))
+		for s in self.hashes:
+			colldb.writeOsuString(s)
 	
-	stars = map.SR[map.mode][int(MODS)]
-	artist = map.artist
-	title = map.title
-	creator = map.creator
-	diffName = map.diffName
-	circleCount = map.circles
-	sliderCount = map.sliders
-	spinnerCount = map.spinners
-	AR = map.AR
-	CS = map.CS
-	HP = map.HP
-	OD = map.OD
-	SV = map.SV
-	drainTimeMs = map.drainTime
-	totalTimeMs = map.totalTime
-	
-	#you can use any filter here. Example: if AR > 8 and creator != "Sotarks". Make sure to quote every string.
-	if stars >= 5.0:
-		dirs.add(map.directory)
+	def __repr__(self):
+		return f'Collection(name={repr(self.name)}, {len(self.hashes)} hashes)'
 
-print('Finished scanning osu!.db. Copying every mapset matching the filter...')
+	def __len__(self):
+		return len(self.hashes)
 
-for folder in (tqdm.tqdm(dirs) if tqdm else dirs):
-	out = os.path.join(OUTPUT, folder)
-	if not os.path.isdir(out):
-		shutil.copytree(os.path.join(INPUT, 'Songs', folder), out)
+class CollectionDb(BinaryFile):
+	def __init__(self, filename=None):
+		self.version = 0
+		self.collections = []
 
-print('Done! Press any key to exit.')
-input()
+		if filename is None:
+			super().__init__()
+		else:
+			self.load(filename)
+
+	def load(self, filename):
+		super().__init__(filename, 'r')
+		self.version = self.readInt()
+		cnt = self.readInt()
+		self.collections = []
+		for i in range(cnt):
+			self.collections.append(Collection.fromDatabase(self))
+
+	def save(self, filename=None):
+		super().__init__(self.filename if filename is None else filename, 'w')
+		self.writeInt(self.version)
+		self.writeInt(len(self.collections))
+		for c in self.collections:
+			c.writeToDatabase(self)
